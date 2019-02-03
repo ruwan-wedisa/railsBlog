bb8a6bb0f93435eae484cf08e52d2d33e7fe65bd
Ruwan <ruwan@localhost.localdomain>
2019-02-03 15:57:19 +0530

    Add/delete comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  #delete comment with post in db dependent
+  has_many :comments,dependent: :destroy
   validates :title,presence:true ,length:{minimum:5}
-  validates :body,presence:true 
+  validates :body,presence:true
 end
